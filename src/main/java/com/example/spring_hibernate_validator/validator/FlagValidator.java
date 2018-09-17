500f25d7c516a2ceea5b36e4a6b42aebc5db86d5
sli <1538852124@qq.com>
2018-09-17 22:46:08 +0800

    perfect README.md

--- src/main/java/com/example/spring_hibernate_validator/validator/FlagValidator.java
+++ src/main/java/com/example/spring_hibernate_validator/validator/FlagValidator.java
@@ -2,7 +2,11 @@ package com.example.spring_hibernate_validator.validator;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
